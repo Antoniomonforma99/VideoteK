ff37ab3eae5b6ce0654ec797533d20982029e07e
antoniomonforma <antoniomonforma@gmail.com>
2021-04-19 22:19:27 +0200

    continue

--- VideoteK/src/app/models/serie.interface.ts
+++ VideoteK/src/app/models/serie.interface.ts
@@ -1,4 +1,4 @@
-export interface Movie {
+export interface Serie {
     uid: string;
     title: string;
     director: string;
--- VideoteK/src/app/views/admin/movies-new/movies-new.component.ts
+++ VideoteK/src/app/views/admin/movies-new/movies-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-movies-new',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesNewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 
   ngOnInit(): void {
   }
