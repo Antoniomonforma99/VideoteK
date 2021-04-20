123f9988b0874186fb0a637b1050d86aca4fc97a
Antonio Joaquín Montero García <montero.gaant20@aulas.triana>
2021-04-20 10:59:58 +0200

    trabajo en clase

--- VideoteK/src/app/views/admin/movies-new/movies-new.component.ts
+++ VideoteK/src/app/views/admin/movies-new/movies-new.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/services/languaje.service';
 
 @Component({
   selector: 'app-movies-new',
@@ -8,9 +10,23 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class MoviesNewComponent implements OnInit {
 
-  constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang('en');
-    this.translate.use('en');
+  langs;
+  currentTranslation = 'es';
+
+  newMovieForm = new FormGroup({
+    title_en: new FormControl(''),
+    description_en: new FormControl(''),
+    title_es: new FormControl(''),
+    description_es: new FormControl(''),
+    title_fr: new FormControl(''),
+    description_fr: new FormControl(''),
+    title_it: new FormControl(''),
+    description_it: new FormControl('')
+  });
+
+
+  constructor(private langService: LanguageService) {
+    this.langs = this.langService.getLangs();
   }
 
   ngOnInit(): void {
